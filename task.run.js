f807b5bee39ca5cab59990ef772c7d881e0ae203
JF <450994392@qq.com>
2018-10-22 12:01:18 +0800

    format js files

--- task.run.js
+++ task.run.js
@@ -1,5 +1,5 @@
 const { run } = require('./src/index');
-const { resolvePath} = require('./src/utils');
+const { resolvePath } = require('./src/utils');
 
 const sourceDir = resolvePath(__dirname, './svgs');
 const outputDir = resolvePath(__dirname, './components/react');
